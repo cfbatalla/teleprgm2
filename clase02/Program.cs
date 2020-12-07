a9f08a199894b721b7b267917e9c3542c3b5cff5
cfbatalla <carlosfranz@ucla.edu.ve>
2020-12-07 16:14:41 -0400

    se sube clase02

---
+++ clase02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Vehiculos;
+
+namespace clase_01{
+    class Program{
+        static void Main(string[] args){
+            string nombre = null;
+            string pais = " ";
+            int edad = 0;
+            bool AptoParaLicencia = false;
+            Console.WriteLine("cual es tu nombre?");
+            nombre = Console.ReadLine();            
+            Console.WriteLine("cual es tu edad?");
+            edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("hola {0}, tienes {1} años de edad.",nombre,edad);
+            if (edad > 16){
+                AptoParaLicencia = true;
+                Console.WriteLine("Lo esperamos mañana para que tramite su licencia");                                
+                //como nace?? Instanciando la clase
+                Vehiculo oVehiculo = new Vehiculo(); 
+                oVehiculo.Arrancar();
+                }
+            else {
+                AptoParaLicencia = false;
+                Console.WriteLine("Ud, no tiene edad suficiente para manejar!");}  
+        }
+    }
+   
+
+   
+}
---
+++ clase02/Vehiculo.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Vehiculos{
+public class Vehiculo{
+    //declaramos atributos de la clase
+    string marca;
+    string color;
+    string placa;
+    double precio;
+    int CantidadDePuertas;
+    string modelo;
+    bool LucesTraserasEncendidad = false;
+    //declaramos metodos de la clase
+    public void Arrancar(){
+        Console.WriteLine("El carro ha arrancado");
+        //aqui van todo lo que implica que el carro haya arrancado
+    }  //{} es un bloque de codigo
+    public void Frenar(){
+         LucesTraserasEncendidad = true;
+    }
+    public void Acelerar(){}
+    public void AcelerarConMusica(){}
+    
+    private void VerificarEstadoBateria(){
+        
+        
+    }
+}
+}
